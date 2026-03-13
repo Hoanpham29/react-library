5dbdae498d12d36dfc65bd260fc438d4004852da
Hoan Pham <hoanpham2911@gmail.com>
2026-03-13 22:46:58 +0700

    feat(review): add review list and star rating component

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ export const App = () => {
           <Route path={'/search'}>
             <SearchBookPage />
           </Route>
-          <Route path={'/checkout'}>
+          <Route path={'/checkout/:bookId'}>
             <BookCheckoutPage/>
           </Route>
         </Switch>
